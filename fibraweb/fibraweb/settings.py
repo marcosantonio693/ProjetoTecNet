649ffc38499fe218263542a0d5e00b7af22ea633
Marcos Silva <marcosanonimo1998@gmail.com>
2025-04-17 18:29:37 -0300

    Preparando deploy no Render

--- fibraweb/fibraweb/settings.py
+++ fibraweb/fibraweb/settings.py
@@ -117,8 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Certifique-se de que esse diretório existe
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
