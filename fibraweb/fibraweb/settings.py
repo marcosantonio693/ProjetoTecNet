1f057b1b568e2160f29190f12c7a62e500140232
Marcos Silva <marcosanonimo1998@gmail.com>
2025-04-17 18:45:55 -0300

    Preparando deploy no Render

--- fibraweb/fibraweb/settings.py
+++ fibraweb/fibraweb/settings.py
@@ -121,9 +121,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # Certifique-se de que esse diretório existe
 ]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 # Exemplo de configuração para Gmail
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
